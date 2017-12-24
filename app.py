b263a0eb86035f0ac6ef9fe2eebee55c74fd5be2
Laurent Vaylet <laurent.vaylet@gmail.com>
2017-12-24 13:44:22 +0100

    fix variable name + format file

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-PORT = int(os.environ.get('PORT', 5000)) # default to 5000
-HOSTNAME = os.environ.get('HOSTNAME', '') # default to empty string
+PORT = int(os.environ.get('PORT', 5000))  # default to 5000
+HOSTNAME = os.environ.get('HOSTNAME', 'NONAME')  # default to NONAME
+
 
 @app.route('/')
 def hello():
     return f'Hello world from Flynn on port {PORT} from container {HOSTNAME}'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=PORT)
