e1fcdac72c2994abaa5bc49282429e7f8c133267
Laurent Vaylet <lvaylet@users.noreply.github.com>
2017-12-24 12:26:44 +0100

    use f-strings for string interpolation

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Bind to PORT if defined, otherwise default to 5000.
-port = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 5000)) # default to 5000
+HOSTNAME = os.environ.get('HOSTNAME', '') # default to empty string
 
 @app.route('/')
 def hello():
-    return 'Hello world from Flynn on port '+str(port)+' from container '+os.environ.get('HOSTNAME','')+"\n"
+    return f'Hello world from Flynn on port {PORT} from container {HOSTNAME}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
