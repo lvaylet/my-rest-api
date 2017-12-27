25713d31a5483d886e2ad6bd394a5039ab6034a6
Laurent Vaylet <laurent.vaylet@gmail.com>
2017-12-27 15:33:18 +0100

    add logging + ability to load app config from file

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 A REST API meant to be deployed with Flynn.
 
-# Development
+# Development Mode
 
 Run a dockerized Redis (https://hub.docker.com/r/_/redis/) with default settings:
 ```
 $ docker run --name redis-rest-api -d -p 6379:6379 redis
 ```
 
-# Production
+# Production Mode
 
 Provision a Redis database from Flynn, with default settings
 """
 
 import os
 import hammock
-# import logging.confg
+import logging
 from flask import Flask, request, render_template, redirect
 from flask_caching import Cache
 from flask_restful import Resource, Api
@@ -25,9 +26,14 @@ from redis import ConnectionError
 
 # Configure Flask application
 app = Flask(__name__)
-# app.config.from_pyfile('config.py')
-# if 'LOGGING' in app.config:
-#     logging.config.dictConfig(app.config['LOGGING'])
+# Load config from local file
+# import logging.config
+# try:
+#     app.config.from_pyfile('app_config.py')
+#     if 'LOGGING' in app.config:
+#         logging.config.dictConfig(app.config['LOGGING'])
+# except IOError:
+#     app.logger.warning("Could not load app_config.py")
 
 # Configure Flask-Caching with Redis
 # Use environment variables in production or default values in development with Dockerized Redis
@@ -51,15 +57,30 @@ LMS_TOKEN = os.environ['LMS_TOKEN']
 LMS = hammock.Hammock('https://talend.talentlms.com/api/v1',
                       auth=(LMS_TOKEN, ''))
 
+# Data
+# FIXME Save in database (Redis, same as caching?)
 todos = {
     'todo1': 'Remember the milk',
     'todo2': 'Change my brakepads',
 }
 
 
+# Logging
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # Have gunicorn capture logging messages from Flask
+        # https://github.com/benoitc/gunicorn/issues/379
+        # ---
+        # In production mode, add log handler to sys.stderr
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
+
 # Errors
 @app.errorhandler(404)
 def page_not_found(e):
+    app.logger.error('Page not found! Rendering error page...')
     return render_template('error.html',
                            message='Page not found!',
                            description='The requested URL was not found on the server.'), 404
@@ -67,9 +88,10 @@ def page_not_found(e):
 
 @app.errorhandler(ConnectionError)
 def connection_error(e):
-    debug_description = "a (dockerized?) <strong>redis-server</strong> is running locally"
-    production_description = "a <strong>redis-server</strong> has been provisioned in Flynn for this app"
-    description = "Please confirm that %s." % (debug_description if app.debug else production_description)
+    debug_description = 'a (dockerized?) <strong>redis-server</strong> is running locally'
+    production_description = 'a <strong>redis-server</strong> has been provisioned in Flynn for this app'
+    description = 'Please confirm that %s.' % (debug_description if app.debug else production_description)
+    app.logger.error(f'Could not connect to the Redis cache! (at {REDIS_URL})')
     return render_template('error.html',
                            message='Could not connect to the Redis cache!',
                            description=description), 500
@@ -78,9 +100,11 @@ def connection_error(e):
 @api.resource('/todos/<string:todo_id>')
 class Todo(Resource):
     def get(self, todo_id):
+        app.logger.info(f'Fetching todo item with ID [{todo_id}]...')
         return {todo_id: todos[todo_id]}
 
     def put(self, todo_id):
+        app.logger.info(f'Creating todo item with ID [{todo_id}]...')
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}, 201
 
@@ -88,6 +112,7 @@ class Todo(Resource):
 @api.resource('/todos')
 class TodoList(Resource):
     def get(self):
+        app.logger.info('Fetching all todo items...')
         return todos
 
 
@@ -95,6 +120,7 @@ class TodoList(Resource):
 class LMSUserList(Resource):
     @cache.cached()
     def get(self):
+        app.logger.info('Fetching users on LMS...')
         return LMS.users.GET().json()
 
 
@@ -102,12 +128,14 @@ class LMSUserList(Resource):
 class LMSCourseList(Resource):
     @cache.cached()
     def get(self):
+        app.logger.info('Fetching courses on LMS...')
         return LMS.courses.GET().json()
 
 
 # Views
 @app.route('/')
 def index():
+    app.logger.info('Redirecting to /todos...')
     return redirect('/todos')
 
 
